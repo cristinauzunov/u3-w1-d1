c8052f937297c5987a4010ed13ce1c64ace72e53
cristinauzunov <cristina.uzunov99@gmail.com>
2026-04-27 15:23:28 +0200

    react.ex

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,10 @@
+//a funzione: più semplici da scrivere
+//a classe: un pò più complessi
+
+//nome funzione === nome file
+
+const ButtonComponent = (props) => {
+  return <button>{props.content}</button>;
+};
+
+export default ButtonComponent;
